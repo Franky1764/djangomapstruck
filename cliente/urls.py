d60e3e5fc0ce728e56537f1fa70d5e4fa299c59b
Daniela Castillo <franky1764@gmail.com>
2024-07-18 00:41:20 -0400

    cambios en modal registro

--- cliente/urls.py
+++ cliente/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
     path('contacto', views.contacto, name='contacto'),
     path('servicios', views.servicios, name='servicios'),
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('about/', views.about_view, name='about'),
-    path('services/', views.services_view, name='services'),
-    path('contact/', views.contact_view, name='contact'),
     path('client-register/', views.client_register_view, name='client_register'),
     path('success/', views.success_view, name='success'),
 ]
